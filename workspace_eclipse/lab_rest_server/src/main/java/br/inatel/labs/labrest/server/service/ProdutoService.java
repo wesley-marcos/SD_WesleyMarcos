40741c4e990cbde07672ad85e1a709b304e1eaf0
wesley-marcos <wesley.marcos@gec.inatel.br>
2023-11-02 12:33:45 -0300

    feat: adicionando o method 'setup()', que adiciona 3 produtos na lista 'produtos'

--- workspace_eclipse/lab_rest_server/src/main/java/br/inatel/labs/labrest/server/service/ProdutoService.java
+++ workspace_eclipse/lab_rest_server/src/main/java/br/inatel/labs/labrest/server/service/ProdutoService.java
@@ -1,13 +1,27 @@
 package br.inatel.labs.labrest.server.service;
 
 import br.inatel.labs.labrest.server.model.Produto;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class ProdutoService {
 
     List<Produto> produtos = new ArrayList<>();
+    @PostConstruct
+    private void setup(){
+        Produto p1 = new Produto(1l, "Furadeira", new BigDecimal(230.00));
+        Produto p2 = new Produto(2l, "Serra Circular", new BigDecimal(500.00));
+        Produto p3 = new Produto(3l, "Parafusadeira", new BigDecimal(400.00));
+
+        produtos.add(p1);
+        produtos.add(p2);
+        produtos.add(p3);
+    }
 
     public List<Produto> findAll(){
         return this.produtos;
