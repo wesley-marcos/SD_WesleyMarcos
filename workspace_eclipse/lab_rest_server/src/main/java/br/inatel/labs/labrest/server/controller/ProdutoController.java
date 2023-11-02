67fdcdf6440b1270f6335b7d3b547fecfd1c471b
wesley-marcos <wesley.marcos@gec.inatel.br>
2023-11-02 14:00:03 -0300

    feat: melhorando a expressividade

--- workspace_eclipse/lab_rest_server/src/main/java/br/inatel/labs/labrest/server/controller/ProdutoController.java
+++ workspace_eclipse/lab_rest_server/src/main/java/br/inatel/labs/labrest/server/controller/ProdutoController.java
@@ -3,10 +3,12 @@ package br.inatel.labs.labrest.server.controller;
 import br.inatel.labs.labrest.server.model.Produto;
 import br.inatel.labs.labrest.server.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,6 +28,12 @@ public class ProdutoController {
     @GetMapping("/{id}")
     public Produto getProdutoById(@PathVariable("id") Long produtoId){
         Optional<Produto> opProduto = service.findById(produtoId);
+
+        if(opProduto.isEmpty()) {
+            String msgErro = String.format("Nehum produto encontrado com id [%s]", produtoId);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, msgErro);
+        }
+
         return opProduto.get();
     }
 }
